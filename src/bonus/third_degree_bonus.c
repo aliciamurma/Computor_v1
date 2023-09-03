b07ec7c7761af4ec9ca6a761dafa73d64043d63e
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2023-09-03 14:53:40 +0200

    Add files via upload

--- src/bonus/third_degree_bonus.c
+++ src/bonus/third_degree_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 14:33:27 by amurcia-          #+#    #+#             */
-/*   Updated: 2023/09/03 14:50:24 by amurcia-         ###   ########.fr       */
+/*   Updated: 2023/09/03 14:53:25 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	ft_third_degree(t_letters let)
             r = cbrt(-q / 2 + delta);
             s = cbrt(-q / 2 - delta);
             t = -let.b / (3 * let.a);
-            printf("The real solution is:\n");
+            printf("The solution is:\n");
             printf("x1 = %.4lf\n", r + s + t);
             printf("x2 = %.4lf\n", -(r + s) / 2 + t);
             printf("x3 = %.4lf\n", -(r + s) / 2 + t);
@@ -48,7 +48,7 @@ void	ft_third_degree(t_letters let)
             r = 2 * cbrt(sqrt(pow(-(q / 2), 2) - pow(p / 3, 3))) * cos(theta / 3);
             s = 2 * cbrt(sqrt(pow(-(q / 2), 2) - pow(p / 3, 3))) * cos((theta + 2 * M_PI) / 3);
             t = 2 * cbrt(sqrt(pow(-(q / 2), 2) - pow(p / 3, 3))) * cos((theta + 4 * M_PI) / 3);
-            printf("The real solution is:\n");
+            printf("The solution is:\n");
             printf("x1 = %.4lf\n", r + s - (let.b / (3 * let.a)));
             printf("x2 = %.4lf + %.4lfi\n", -(r + s) / 2 - (let.b / (3 * let.a)), (r - s) * sqrt(3) / 2);
             printf("x3 = %.4lf - %.4lfi\n", -(r + s) / 2 - (let.b / (3 * let.a)), (r - s) * sqrt(3) / 2);
@@ -59,7 +59,7 @@ void	ft_third_degree(t_letters let)
         r = -let.b / (3 * let.a);
         s = -let.b / (3 * let.a);
         t = -let.b / (3 * let.a);
-        printf("Una raíz real y dos raíces complejas conjugadas:\n");
+        printf("The solution is:\n");
         printf("x1 = %.4lf\n", r + s + t);
         printf("x2 = %.4lf + %.4lfi\n", -(r + s) / 2, sqrt(3) * (r - s) / 2);
         printf("x3 = %.4lf - %.4lfi\n", -(r + s) / 2, sqrt(3) * (r - s) / 2);
@@ -70,7 +70,7 @@ void	ft_third_degree(t_letters let)
         s = (2 * pow(let.b, 3) - 9 * let.a * let.b * let.c + 27 * pow(let.a, 2) * let.d) / (27 * pow(let.a, 3));
         t = sqrt(-(p / 3));
         u = acos(-(s / (2 * t)));
-        printf("Tres raíces reales iguales:\n");
+        printf("The solution is:\n");
         printf("x1 = %.4lf\n", -2 * t * cos(u / 3) - (let.b / (3 * let.a)));
         printf("x2 = %.4lf\n", 2 * t * cos((u + 2 * M_PI) / 3) - (let.b / (3 * let.a)));
         printf("x3 = %.4lf\n", 2 * t * cos((u + 4 * M_PI) / 3) - (let.b / (3 * let.a)));
